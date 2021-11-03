d3fb31710de2f93d0c1809a4afb2fd32fd5a4a3d
Alen Vodopija2 <alen.vodopija2@gmail.com>
2021-11-03 11:35:14 -0700

    Card number

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -12,7 +12,8 @@ class TicketsController < ApplicationController
       end
 
     def create
-      
+      @card_number_json = params.require(:offer).permit(:card_number)
+      @card_number = @card_number_json['card_number']
       @ticket_params = params.require(:offer).permit(:offer_id,:user_id)
       @ticket = Ticket.new(@ticket_params)
       @offer = Offer.find(@ticket.offer_id)
@@ -21,14 +22,15 @@ class TicketsController < ApplicationController
         redirect_to root_path
         flash[:warning] = "No seats left."
 
-       elsif @ticket.save 
+       elsif @ticket.save &&  (@card_number_json['card_number'].to_i.digits.length() == 14)
          @offer.number_of_seats -=1
           @offer.save
          flash[:success] = "Ticekt bought"
          redirect_to root_path
 
        else
-         render 'new'
+        redirect_to root_path
+        flash[:danger] = "Please insert a 14 digit card number to buy a ticket."
       end
       
     end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 require "view_component/engine"
+require "json"
 
 
 # Require the gems listed in Gemfile, including any gems
